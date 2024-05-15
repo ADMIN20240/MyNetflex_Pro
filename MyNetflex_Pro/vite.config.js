a507debfb37c39aaf63726d36c90fccc16bdce1c
ADMIN20240 <aanole445@gmail.com>
2024-05-15 01:49:06 -0500

    update the base  homepage

--- MyNetflex_Pro/vite.config.js
+++ MyNetflex_Pro/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MyNetflex_pro/",
+  base: "/MyNetflex_Pro/",
 });
 
 // vite.config.js
