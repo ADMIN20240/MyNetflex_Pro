4b2cbbf3f44c2781a25ed37eb7bf74d97b4d6257
ADMIN20240 <aanole445@gmail.com>
2024-05-14 23:57:51 -0500

    Add built files for deployment

--- MyNetflex_Pro/vite.config.js
+++ MyNetflex_Pro/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/MyNetflex_pro/",
+});
 
 // vite.config.js
 // import { defineConfig } from 'vite';
