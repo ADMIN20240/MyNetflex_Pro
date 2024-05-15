bd3b360b65c363253d6b6974e29c096ed905f372
ADMIN20240 <aanole445@gmail.com>
2024-05-14 23:27:00 -0500

    change assets index file status

--- MyNetflex_Pro/vite.config.js
+++ MyNetflex_Pro/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
+// vite.config.js
+// import { defineConfig } from 'vite';
+// export default defineConfig({
+//   server: {
+//     port: 3000,
+//     title: "Netflix Title", // Change the title here
+//   },
+// });
